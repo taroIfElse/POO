a48fb6b2f1e85e736e16edfc75d3415a0180ce72
Lautaro Avecilla <lautaroaveclla2020@gmail.com>
2022-07-01 12:20:43 -0300

    se añadieron forms(listar y agregar) a user

--- 2-add_PerfilesFormularios.sql
+++ 2-add_PerfilesFormularios.sql
@@ -20,3 +20,15 @@ values (
 	(SELECT Id FROM Formularios WHERE NombreSistema = 'agregarToolStripMenuItem')
 );
 
+/*USER NORMAL*/
+
+INSERT INTO PerfilFormulario (Perfil_Id, Formulario_Id)
+values (
+	(SELECT Id FROM Perfiles WHERE Nombre = 'Cliente'),
+	(SELECT Id FROM Formularios WHERE NombreSistema = 'agregarToolStripMenuItem')
+);
+INSERT INTO PerfilFormulario (Perfil_Id, Formulario_Id)
+values (
+	(SELECT Id FROM Perfiles WHERE Nombre = 'Cliente'),
+	(SELECT Id FROM Formularios WHERE NombreSistema = 'listarClientesToolStripMenuItem')
+);
