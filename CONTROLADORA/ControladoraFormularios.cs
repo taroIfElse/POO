99e0eb8603af0ddc3e9829f5adf72c6a5407ed90
Florencia Mon <flor_montini@hotmail.com>
2022-07-27 15:43:03 -0300

    Cambios en controladora

--- CONTROLADORA/ControladoraFormularios.cs
+++ CONTROLADORA/ControladoraFormularios.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CONTROLADORA
-{ //CLASE
+{ //Clase
     public class ControladoraFormularios
     {
         private static ControladoraFormularios _instancia;
