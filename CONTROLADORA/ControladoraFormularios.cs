59abf7e08922a4cc2a37d359f26bba67b9ab9ecc
Jona Pierini <jonapierini@gmail.com>
2022-07-27 15:31:28 -0300

    mejora de visualizacion de codigo

--- CONTROLADORA/ControladoraFormularios.cs
+++ CONTROLADORA/ControladoraFormularios.cs
@@ -5,18 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CONTROLADORA
-{
+{ //CLASE
     public class ControladoraFormularios
     {
         private static ControladoraFormularios _instancia;
 
-        private ControladoraFormularios() { }
+        private ControladoraFormularios() { } //CONSTRUCTOR
 
         public static ControladoraFormularios obtener_instancia()
         {
             if (_instancia == null)
             {
-                _instancia = new ControladoraFormularios();
+                _instancia = new ControladoraFormularios(); // INSTANCIA DE CONSTRUCTOR
             }
             return _instancia;
         }
--- MODELO/Contexto.Designer.cs
+++ MODELO/Contexto.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\LAvecilla\OneDrive - ENDAVA\Desktop\POO FINAL\POO-UAI-Template-final-project\MODELO\Contexto.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\JPierini\OneDrive - ENDAVA\Desktop\POO\POO\MODELO\Contexto.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
